faeddb0dfdc95786d648c317002c411a2670f2d9
kaichi0618 <kaichi.1998.0618@gmail.com>
2021-11-12 06:04:20 +0000

    [start]gem,routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+  
+  root to: 'homes#top'
+  resources users, only: [:show, :edit, :update]
+  patch 'unsubscribe' => 'users#unsubscribe'
+  get 'confirm' => 'users#confirm'
+  resources shops do
+    resources :shop_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
