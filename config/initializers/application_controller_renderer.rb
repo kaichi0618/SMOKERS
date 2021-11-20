9217bc4f80b13bffce2a2ad98bb2d519f299635a
kaichi0618 <kaichi.1998.0618@gmail.com>
2021-11-20 03:20:43 +0000

    [add]refile_secret.key

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '6a1368287f19bd31da747c828a9d67396682df2a517ffc2b6e1a4a1ef0ef97bfbc8ae66eddafa02ea3c4af6f955f3a0aa724dcfca428706e7f27d7115ec36d78'
