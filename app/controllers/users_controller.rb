02eb95ad7f1d26b04756e7e77f70c0420ade8ea1
kaichi0618 <kaichi.1998.0618@gmail.com>
2021-12-02 14:19:04 +0000

    [improve]コードの簡略化

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,16 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:favorites]
+  before_action :set_user, only: [:favorites, :show, :update, :edit]
 
   def show
-    @user = User.find(params[:id])
     @shops = @user.shops
     favorites = Favorite.where(user_id: @user.id).pluck(:shop_id)
     @favorite_shops = Shop.find(favorites)
   end
 
   def edit
-     @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to user_path(@user), notice: "You have updated user successfully."
     else
